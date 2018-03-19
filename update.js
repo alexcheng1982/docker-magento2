737eaea05dde507d53c0e2979a5370f86d254456
Fu Cheng <alexcheng1982@gmail.com>
2018-03-20 08:45:38 +1300

    Upgrade to Magento 2.1.12

--- update.js
+++ update.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const commonOptions = {
   phpVersion: '7.0.24',
-  magento2Version: '2.1.11',
+  magento2Version: '2.1.12',
 };
 
 function readPartial(profile, section) {
