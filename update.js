98ca3b1281cda8a449595421997b249e4da582aa
Fu Cheng <alexcheng1982@gmail.com>
2018-10-01 21:12:23 +1300

    Magento 2.2.6

--- update.js
+++ update.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const commonOptions = {
   phpVersion: '7.1.11',
-  magento2Version: '2.2.5',
+  magento2Version: '2.2.6',
 };
 
 function readPartial(profile, section) {
