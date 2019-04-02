907623b8331bd48ee37fe3c729562485249c1d31
Fu Cheng <alexcheng1982@gmail.com>
2019-04-03 11:24:44 +1300

    Magento 2.1.17

--- update.js
+++ update.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const commonOptions = {
   phpVersion: '7.0.32',
-  magento2Version: '2.1.16',
+  magento2Version: '2.1.17',
 };
 
 function readPartial(profile, section) {
